ef6f17b1b52ec60fa50417ec0e580c5fc2310140
cj-yy <72062723+cj-yy@users.noreply.github.com>
2020-10-12 19:02:48 +0800

    Update painted eggshell.js

--- painted eggshell.js
+++ painted eggshell.js
@@ -1,1 +1,17 @@
 var e,i ;
+e = document.getElementById("control")
+i = 0;
+e.onclick = function(){
+  if (i == 0){
+    alert("wulalaala");
+    i++;
+  } else if (i == 1){
+    alert("wululululu");
+    i++;
+  } else {
+    alert("wuwuwuwuwuw");
+    i = 0;
+  }
+  
+  return false;
+}
